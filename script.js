ee4f2e8b8e5d8974600accfb1d0bc8c90d1c981c
prakharmi <mishraprakharsuper@gmail.com>
2025-01-13 15:49:57 +0530

    added form page

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function handleCredentialResponse(response) {
   console.log("Encoded JWT ID token: " + response.credential);
   
-  // Decode the token to get user information (e.g., name, email)
   const payload = JSON.parse(atob(response.credential.split('.')[1]));
   console.log("User Info:", payload);
   alert(`Hello, ${payload.name}!`);
   
-  // Redirect to the success page
-  window.location.href = "loginsuccess.html"; // Update with the correct path to your success page
+  window.location.href = "detailspage.html";
 }
 
-// Initialize Google Sign-In
 window.onload = () => {
   google.accounts.id.initialize({
       client_id: "866863334708-6o7pat7hkajrhve0s50tv1cpks0fnvbu.apps.googleusercontent.com",
       callback: handleCredentialResponse,
   });
   google.accounts.id.renderButton(
-      document.getElementById("google-signin-button"), // Element to render the button
-      { theme: "outline", size: "large" } // Button customization
+      document.getElementById("google-signin-button"),
+      { theme: "outline", size: "large" }
   );
-  google.accounts.id.prompt(); // Automatically prompt the user
+  google.accounts.id.prompt();
 };
