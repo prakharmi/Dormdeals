5f4f315caabbbfece0a187d59e270abdaa3cc5ff
prakharmi <mishraprakharsuper@gmail.com>
2025-03-16 00:07:37 +0530

    Added the database details such as table schemas for reference

---
+++ Database Details/product_images_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product_images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    image_url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
---
+++ Database Details/users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    college VARCHAR(255) NOT NULL,
+    mobile_number VARCHAR(20) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
