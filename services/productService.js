fa5262bf381e5ff4c4d5a0ef7698a9c3a39d7edd
prakharmi <mishraprakharsuper@gmail.com>
2025-04-14 01:39:38 +0530

    fixed case sensitivity issue due to case sensitivity of linux

--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,5 @@
-const Product = require("../models/Product");
-const ProductImage = require("../models/ProductImage");
+const Product = require("../models/product");
+const ProductImage = require("../models/productimage");
 const cloudinary = require("../config/cloudinary");
 const fs = require('fs');
 const path = require('path');
--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 
 class UserService {
   static async checkUserExists(email) {
